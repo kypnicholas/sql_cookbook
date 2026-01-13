c40ab482e20ebaca2eb578dd2a7d35bc18d4650f
kypnicholas <kypnicholas@gmail.com>
2026-01-13 11:13:07 +0100

    cleanup unused seed files

--- migrations/001_create_customers_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS customers (
-  customer_id SERIAL PRIMARY KEY,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  email TEXT UNIQUE,
-  country TEXT,
-  total_spent NUMERIC DEFAULT 0
-);
