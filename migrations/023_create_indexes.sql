a449a1ecfe5e5857bdee39738b14f4cca0bf0ae7
kypnicholas <kypnicholas@gmail.com>
2026-07-05 13:40:52 +0200

    Add index creation migration for invoice and invoice_line tables to enhance query performance

--- migrations/023_create_indexes.sql
+++ migrations/023_create_indexes.sql
@@ -1,1 +1,7 @@
-# WIP
+
+# This migration creates indexes on the invoice and invoice_line tables to improve query performance. 
+# The idx_invoice_invoice_date index is created on the invoice_date column of the invoice table, 
+# while the idx_invoice_line_track_id index is created on the track_id column of the invoice_line table.
+
+CREATE INDEX idx_invoice_invoice_date ON invoice(invoice_date);
+CREATE INDEX idx_invoice_line_track_id ON invoice_line(track_id);
